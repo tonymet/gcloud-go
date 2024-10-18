6cfe956db92ae5e0d1ca968b0126ad5814dae31c
Anthony Metzidis <anthony.metzidis@gmail.com>
2024-10-17 22:03:22 -0700

    experimental / broken concurrent fs support

--- fs/fs.go
+++ fs/fs.go
@@ -15,22 +15,55 @@ type ShaRec struct {
 type ShaList []ShaRec
 
 func ShaFiles(dirname, tempDir string) (ShaList, error) {
+	type jobArgs struct {
+		path string
+		f    fs.FileInfo
+		err  error
+	}
 	var curShaList = make(ShaList, 0, 10)
-	shaProcess := func(path string, f fs.FileInfo, err error) error {
-		if f.IsDir() {
-			return nil
-		}
-		if h, err := fbcompress.CompressAndHashFile(path, ppath.Join(tempDir, f.Name())); err != nil {
-			panic(err)
-		} else {
-			s := ShaRec{ppath.Join("/", path), fbcompress.TextSum(h)}
-			if err := os.Rename(ppath.Join(tempDir, f.Name()), ppath.Join(tempDir, s.Shasum)); err != nil {
-				panic(err)
+	shaProcess := func(jobs <-chan jobArgs, results chan<- ShaRec) error {
+		for j := range jobs {
+			path, f, _ := j.path, j.f, j.err
+			if f.IsDir() {
+				continue
 			}
-			curShaList = append(curShaList, s)
+			if h, err := fbcompress.CompressAndHashFile(path, ppath.Join(tempDir, f.Name())); err != nil {
+				panic(err)
+			} else {
+				s := ShaRec{ppath.Join("/", path), fbcompress.TextSum(h)}
+				if err := os.Rename(ppath.Join(tempDir, f.Name()), ppath.Join(tempDir, s.Shasum)); err != nil {
+					panic(err)
+				}
+				results <- s
+			}
 		}
 		return nil
 	} // walk files and update
-	filepath.Walk(dirname, shaProcess)
+
+	// set up
+	jobs, results := make(chan jobArgs), make(chan ShaRec)
+
+	queueFile := func(path string, f fs.FileInfo, err error) error {
+		jobs <- jobArgs{path, f, err}
+		return nil
+	}
+
+	for w := 1; w <= 4; w++ {
+		go shaProcess(jobs, results)
+	}
+
+	go func() {
+		filepath.Walk(dirname, queueFile)
+		close(jobs)
+	}()
+
+	for {
+		if s, ok := <-results; !ok {
+			break
+		} else {
+			curShaList = append(curShaList, s)
+		}
+	}
+	// close jobs
 	return curShaList, nil
 }
