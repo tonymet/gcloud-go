1426cf0f8485c40ebbaa5007caf35945e3daa84c
Anthony Metzidis <anthony.metzidis@gmail.com>
2024-10-18 15:57:31 -0700

    release

--- github-release.sh
+++ github-release.sh
@@ -11,6 +11,7 @@ github_release(){
     owner=tonymet
     repo="gcloud-go"
     COMMIT=$(git log -1 --pretty=%h)
+    SHA=$(git log -1 --pretty=%H)
     VERSION=$(date +%Y-%m-%d)-${COMMIT}
     if [[ -z $GH_TOKEN ]]; then
         echo "GH_TOKEN is unset"
@@ -19,13 +20,13 @@ github_release(){
     TAG=$VERSION
     echo "creating release version=$VERSION"
     res=$(\
-        curl -s -L -f \
+        curl --verbose -s -L -f \
     -X POST \
     -H "Accept: application/vnd.github+json" \
     -H "Authorization: Bearer $GH_TOKEN" \
     -H "X-GitHub-Api-Version: 2022-11-28" \
     https://api.github.com/repos/${owner}/${repo}/releases \
-    -d "{\"tag_name\":\"$TAG\",\"target_commitish\":\"${COMMIT}\",\"name\":\"$TAG\",\"body\":\"gcloud-go cli release\",\"draft\":false,\"prerelease\":false,\"generate_release_notes\":false}"\
+    -d "{\"tag_name\":\"${TAG}\",\"target_commitish\":\"master\",\"name\":\"${TAG}\",\"body\":\"gcloud-go cli release\",\"draft\":false,\"prerelease\":false,\"generate_release_notes\":false}"\
     )
     if [[ $? -ne 0 ]]; then
         echo "ERROR: create release fail"
